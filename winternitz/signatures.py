23f6f5388b6534206d92345fe90bb822edc8a0d4
Harald Heckmann <harald.heckmann93@web.de>
2019-02-28 17:56:02 +0100

    finished __init__, added abstract class, added _generatePubKey function

--- winternitz/signatures.py
+++ winternitz/signatures.py
@@ -1,21 +1,43 @@
+from abc import ABCMeta, abstractmethod
 from hashlib import blake2b
 from math import ceil, floor, log2
-from typing import List, Optional
+from os import urandom
+from typing import Any, List, Optional
+
+
+# Abstract definition of OTS class
+class AbstractOTS(object, metaclass=ABCMeta):
+    @abstractmethod
+    def sign(message: bytes) -> dict:
+        raise NotImplementedError("sign is essential for WOTS signatures")
+
+    @abstractmethod
+    def verify(message: bytes, signature: List[bytes]) -> bool:
+        raise NotImplementedError("verify is essential for WOTS signatures")
+
+    @abstractmethod
+    def __eq__(self, obj: Any) -> bool:
+        raise NotImplementedError("Equality checks are required")
+
+    @abstractmethod
+    def __ne__(self, obj: Any) -> bool:
+        raise NotImplementedError("Non-equality checks are required")
 
 
 # Paper describing WOTS: https://eprint.iacr.org/2011/191.pdf
 # "On the Security of the Winternitz One-Time Signature Scheme"
 # Note: This Class calculates with the logarithm to the base 2 of the
 # Winternitz parameter. This is dues to the fact, that machine store numbers
-# in binary representation, numbers likes w = 5 therefore cannot be realized
+# in binary representation, numbers like w = 5 therefore cannot be realized
 # and result in w = 2**ceil(log2(w)), which would be w = 8.
 # Other than that it is easier to caluclate.
-class WOTS(object):
-    def __init__(self, w_log2: int,
-                 hashalgo: List[int] = blake2b,
-                 digestsize=256,
+class WOTS(AbstractOTS):
+    def __init__(self,
+                 w_log2: int,
+                 hash_function: Any = blake2b,  # TODO: correct Type
+                 digestsize: int = 256,
                  privkey: Optional[List[bytes]] = None,
-                 pubkey: Optional[List[bytes]] = None):
+                 pubkey: Optional[List[bytes]] = None) -> None:
 
         if w_log2 > digestsize:
             raise ValueError("The winternitz parameter must be lower than\
@@ -29,67 +51,86 @@ class WOTS(object):
                                    1 + w_log2) / w_log2)
         self.__key_count = self.__msg_key_count + self.__cs_key_count
 
+        # Hashing algorithm
+        self.__hash_function = hash_function
+        self.__digest_size = digestsize
+
+        # Keys
+        self.__privkey = []
+        self.__pubkey = []
+        hash_bytes = int(ceil(digestsize / 8))
+
         # generate random privkey, derive pubkey
         if privkey is None and pubkey is None:
-            # TODO:
-            pass
+            privkey = [pkey for pkey in urandom(hash_bytes)]
+            self._derivePubKey()
         # derive pubkey
         elif privkey is not None:
-            # TODO:
-            pass
+            if len(privkey) != self.__key_count:
+                raise ValueError("Provided private key length does not match to\
+                                  provided winternitz parameter")
+
+            self.__privkey = privkey.copy()
+            self._derivePubkey()
         # set pubkey
         else:
-            self.__privkey = None
+            if len(pubkey) != self.__cs_key_count:
+                raise ValueError("Provided private key length does not match to\
+                                  provided winternitz parameter")
 
-        # Cases: privkey = None and pub is set. set self.privkey to None
-        # privkey set pubkey set just derive new pubkey
-        # privkey not set pubkey not set just generate priv and derive pub
-        pass
+            for elem in filter(lambda t: len(t) != hash_bytes, pubkey):
+                raise ValueError("Length of public key hashes does not match\
+                                  with the provided digestsize")
 
-    def __repr__():
+            self.__pubkey = pubkey.copy()
+
+    def __repr__(self) -> str:
         # TODO
         pass
 
-    def __str__():
+    def __str__(self) -> str:
         # TODO
         pass
 
-    def __eq__():
+    def __eq__(self, obj: Any) -> bool:
         # TODO
         pass
 
-    def __ne__():
+    def __ne__(self, obj: Any) -> bool:
         # TODO
         pass
 
     @property
-    def privkey(self):
+    def privkey(self) -> List[bytes]:
         # TODO: get privkey
         pass
 
     @property
-    def pubkey(self):
+    def pubkey(self) -> List[bytes]:
         # TODO: get privkey
         pass
 
     @property
-    def w(self):
+    def w(self) -> int:
         return self.__w_log2
         # TODO: get Winternitz parameter
         pass
 
     @property
-    def hashalgo(self):
+    def hashalgo(self) -> Any:  # TODO: correct Type
         # TODO: get hashalgorithm
         pass
 
     @property
-    def digestsize(self):
+    def digestsize(self) -> int:
         # TODO: get digestsize
         pass
 
-    def sign(message: bytes)\
-            -> dict:
+    def _derivePubKey(self) -> None:
+        # TODO:
+        pass
+
+    def sign(message: bytes) -> dict:
         # TODO: implement sign algorithm
         # Check if privkey none, throw exception  if it is
         pass
@@ -102,47 +143,54 @@ class WOTS(object):
 # Paper descirbing WOTS+: https://eprint.iacr.org/2017/965.pdf
 # "W-OTS+ – Shorter Signatures for Hash-BasedSignature Schemes"
 class WOTSPLUS(WOTS):
-    def __init__(self, w_log2: int,
-                 hashalgo: List[int] = blake2b,
+    def __init__(self,
+                 w_log2: int,
+                 hash_function: Any = blake2b,  # TODO: correct Type
                  # TODO: Pseudo Random Function for Key and BM derivation
-                 digestsize=256,
+                 digestsize: int = 256,
                  privkey: Optional[List[bytes]] = None,
                  pubkey: Optional[List[bytes]] = None,
                  seed: Optional[bytes] = None):
-        super().__init__(w_log2, privkey, hashalgo, digestsize)
+
+        super().__init__(w_log2, hash_function=hash_function,
+                         digestsize=digestsize, privkey=privkey,
+                         pubkey=pubkey)
         # TODO
         # Only init seed if it is not None
         # Store prf
         pass
 
-    def __repr__():
+    def __repr__(self) -> str:
         # TODO
         pass
 
-    def __str__():
+    def __str__(self) -> str:
         # TODO
         pass
 
-    def __eq__():
+    def __eq__(self, obj) -> bool:
         # TODO
         pass
 
-    def __ne__():
+    def __ne__(self, obj) -> bool:
         # TODO
+        pass
+
+    def _derivePubKey(self) -> None:
+        # TODO:
         pass
 
     @property
-    def seed():
+    def seed(self) -> bytes:
         # TODO: get seed
         pass
 
     @property
-    def prf():
+    def prf(self):
         # TODO: get pseudo random function
         pass
 
-    def sign(message: bytes)\
-            -> dict:
+    def sign(message: bytes) -> dict:
         # TODO: implement sign algorithm
         # Check if privkey none, throw exception  if it is
         pass
